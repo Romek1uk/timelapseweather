524718ea28133bc3b90cf40edc0a8ad3a45bb684
Roman Kolacz <rk476@cam.ac.uk>
2014-11-10 23:29:56 +0000

    Added averageColour

--- src/intro/images/ImageMetricGetter.java
+++ src/intro/images/ImageMetricGetter.java
@@ -1,5 +1,6 @@
 package intro.images;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
@@ -9,11 +10,41 @@ import javax.imageio.ImageIO;
 
 public class ImageMetricGetter
 {
+	private static byte[] getPixels(BufferedImage image)
+	{
+		return ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+	}
+	
+	public static Color averageColour(BufferedImage image)
+	{
+		int pixellength = image.getAlphaRaster() == null ? 3 : 4;
+		
+		byte[] pixels = getPixels(image);
+		
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		
+		for(int i = 0; i < pixels.length; i += 3)
+		{
+			if (pixellength == 4)
+			{
+				i++;
+			}
+			
+			r += pixels[i] & 0xFF;
+			g += pixels[i + 1] & 0xFF;
+			b += pixels[i + 2] & 0xFF;
+		}
+		
+		return new Color(r/pixels.length*pixellength, g/pixels.length*pixellength, b/pixels.length*pixellength);
+	}
+	
 	public static double averageBrightness(BufferedImage image)
 	{
 		int pixellength = image.getAlphaRaster() == null ? 3 : 4;
 		
-		byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		byte[] pixels = getPixels(image);
 		
 		double brightness = 0;
 		
@@ -51,5 +82,6 @@ public class ImageMetricGetter
 		
 		double brightness = averageBrightness(image);
 		System.out.println(brightness);
+		System.out.println(averageColour(image));
 	}
 }
