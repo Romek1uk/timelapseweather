36574b1c330f2f96e885d519bbb0640b1707b40a
Roman Kolacz <rk476@cam.ac.uk>
2015-02-11 15:03:49 +0000

    Added mail sending stuff for errors (WIP- not currently working)

---
+++ src/rk476/timelapseweather/dataharvester/EmailSender.java
@@ -0,0 +1,64 @@
+package rk476.timelapseweather.dataharvester;
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailSender
+{
+   public static void sendEmail(String content)
+   {    
+      // Recipient's email ID needs to be mentioned.
+      String to = "rk476@cam.ac.uk";
+
+      // Sender's email ID needs to be mentioned
+      String from = "rk476@cam.ac.uk";
+
+      // Assuming you are sending email from localhost
+      String host = "localhost";
+
+      // Get system properties
+      Properties properties = System.getProperties();
+
+      // Setup mail server
+      properties.setProperty("mail.smtp.host", host);
+
+      // Get the default Session object.
+      Session session = Session.getDefaultInstance(properties);
+
+      try{
+         // Create a default MimeMessage object.
+         MimeMessage message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.addRecipient(Message.RecipientType.TO,
+                                  new InternetAddress(to));
+
+         // Set Subject: header field
+         message.setSubject("Harvester Error!");
+
+         // Now set the actual message
+         message.setText(content);
+
+         // Send message
+         Transport.send(message);
+         System.out.println("Sent message successfully....");
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
+   
+   public static void main(String[] args) {
+ 	  sendEmail("test");
+   }
+   
+}
