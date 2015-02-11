f2ffbbaf6b4a9ac6222c9146299bcdf06c6a52cf
Roman Kolacz <rk476@cam.ac.uk>
2015-02-11 16:41:12 +0000

    Added new row for which split

--- src/rk476/timelapseweather/dataharvester/data/CsvManipulator.java
+++ src/rk476/timelapseweather/dataharvester/data/CsvManipulator.java
@@ -16,102 +16,122 @@ import java.util.Random;
 
 public class CsvManipulator {
 
-	private String _inputFile;
+    private String _inputFile;
 
-	public String getInputFile() {
-		return _inputFile;
+    public String getInputFile() {
+	return _inputFile;
+    }
+
+    private static Random _random = new Random();
+
+    public CsvManipulator(String file) {
+	_inputFile = file;
+    }
+
+    private String addValueToLine(String line, int col, String content) {
+	LinkedList<String> list = new LinkedList<String>(Arrays.asList(line.split(",")));
+	list.add(col, content);
+
+	String ret = "";
+	for (String s : list) {
+	    ret += s + ",";
 	}
 
-	private static Random _random = new Random();
+	return ret.substring(0, ret.length() - 1);
+    }
 
-	public CsvManipulator(String file) {
-		_inputFile = file;
+    public void addColumnWithValue(int columnNumber, String title, String value) throws IOException {
+	BufferedReader reader = new BufferedReader(new FileReader(_inputFile));
+
+	ArrayList<String> lines = new ArrayList<String>();
+
+	String line;
+	while ((line = reader.readLine()) != null) {
+	    lines.add(line);
 	}
-	
-	public void addColumnWithValue(int columnNumber, String title, String value) throws IOException {
-	    BufferedReader reader = new BufferedReader(new FileReader(_inputFile));
-	    
-	    ArrayList<String> lines = new ArrayList<String>();
-	    
-	    String line;
-	    while ((line = reader.readLine()) != null) {
-		lines.add(line);
+
+	reader.close();
+
+	// Header
+	lines.set(0, addValueToLine(lines.get(0), columnNumber, title));
+
+	for (int i = 1; i < lines.size(); i++) { // Ignore header
+	    lines.set(i, addValueToLine(lines.get(i), columnNumber, value));
+	}
+
+	// TODO: Duplicate code with replaceEntryOnLine
+	BufferedWriter writer = new BufferedWriter(new FileWriter(_inputFile));
+	for (String s : lines) {
+	    writer.write(s + "\n");
+	}
+	writer.close();
+    }
+
+    public void replaceEntryOnLine(int row, int column, String value) throws IOException {
+	BufferedReader reader = new BufferedReader(new FileReader(_inputFile));
+	String content = "";
+
+	row = row + 1; // ignore headers
+
+	ArrayList<String> lines = new ArrayList<String>();
+
+	String line;
+	while ((line = reader.readLine()) != null) {
+	    lines.add(line);
+	}
+
+	reader.close();
+
+	content = lines.get(row);
+
+	String[] split = content.split(",");
+	split[column] = value;
+
+	content = "";
+	for (String s : split) {
+	    content += s + ",";
+	}
+	content = content.substring(0, content.length() - 1); // remove last
+							      // comma
+
+	lines.set(row, content);
+
+	BufferedWriter writer = new BufferedWriter(new FileWriter(_inputFile));
+	for (String s : lines) {
+	    writer.write(s + "\n");
+	}
+	writer.close();
+    }
+
+    public void splitCsvFile(int quantity, String newDirectory) throws IOException {
+	// create files
+	CsvWriter[] trainfiles = new CsvWriter[quantity];
+	CsvWriter[] testfiles = new CsvWriter[quantity];
+
+	for (int i = 0; i < quantity; i++) {
+	    trainfiles[i] = new CsvWriter(newDirectory + "train" + i + ".csv");
+	    testfiles[i] = new CsvWriter(newDirectory + "test" + i + ".csv");
+	}
+
+	BufferedReader reader = new BufferedReader(new FileReader(_inputFile));
+
+	String line;
+	while ((line = reader.readLine()) != null) {
+	    int val = _random.nextInt(quantity);
+
+	    for (int i = 0; i < quantity; i++) {
+		if (i == val)
+		    testfiles[val].addLine(line);
+		else
+		    trainfiles[val].addLine(line);
 	    }
-	    
-	    reader.close();
-	    
-	    // Header
-	    line = lines.remove(0);
-	    String[] split = line.split(",");
-	    LinkedList<String> list = new LinkedList<String>(Arrays.asList(split));
-	    list.add(columnNumber, title);
-	    
-	    for (String s : lines) {
-		
-	    }
-	    
 	}
 
-	public void replaceEntryOnLine(int row, int column, String value) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(_inputFile));		
-		String content = "";
-		
-		row = row + 1; // ignore headers
-		
-		ArrayList<String> lines = new ArrayList<String>();
-		
-		String line;
-		while ((line = reader.readLine()) != null) {
-			lines.add(line);
-		}
-		
-		reader.close();
-		
-		content = lines.get(row);
-		
-		String[] split = content.split(",");
-		split[column] = value;
-		
-		content = "";
-		for(String s : split) {
-			content += s + ",";
-		}
-		content = content.substring(0, content.length() - 1); // remove last comma
-		
-		lines.set(row, content);
-				
-		BufferedWriter writer = new BufferedWriter(new FileWriter(_inputFile));
-		for (String s : lines) {
-			writer.write(s + "\n");
-		}
-		writer.close();
+	reader.close();
+
+	for (int i = 0; i < quantity; i++) {
+	    new CsvManipulator(newDirectory + "train" + i + ".csv").addColumnWithValue(0, "split", Integer.toString(i));
+	    new CsvManipulator(newDirectory + "test" + i + ".csv").addColumnWithValue(0, "split", Integer.toString(i));
 	}
-	
-	public void splitCsvFile(int quantity,
-			String newDirectory) throws IOException {
-		// create files
-		CsvWriter[] trainfiles = new CsvWriter[quantity];
-		CsvWriter[] testfiles = new CsvWriter[quantity];
-
-		for (int i = 0; i < quantity; i++) {
-			trainfiles[i] = new CsvWriter(newDirectory + "train" + i + ".csv");
-			testfiles[i] = new CsvWriter(newDirectory + "test" + i + ".csv");
-		}
-
-		BufferedReader reader = new BufferedReader(new FileReader(_inputFile));
-
-		String line;
-		while ((line = reader.readLine()) != null) {
-			int val = _random.nextInt(quantity);
-
-			for (int i = 0; i < quantity; i++) {
-				if (i == val)
-					testfiles[val].addLine(line);
-				else
-					trainfiles[val].addLine(line);
-			}
-		}
-
-		reader.close();
-	}
+    }
 }
