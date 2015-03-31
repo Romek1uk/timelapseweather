502861047752518d264d3e9c9dd7313f9a9519e4
Roman Kolacz <rk476@cam.ac.uk>
2015-03-31 11:49:46 +0100

    Fixed so continious histograms are now correct range

--- src/rk476/timelapseweather/evaluation/DataDescriptor.java
+++ src/rk476/timelapseweather/evaluation/DataDescriptor.java
@@ -41,7 +41,7 @@ public class DataDescriptor {
 	    System.out.println(s + ": " + data.get(s) + " (" + String.format("%.2f", (double) data.get(s) * 100.0 / inputs.size()) + "%)");
 	}
     }
-    
+
     public static class IntStringComparator implements Comparator<String> {
 	public int compare(String obj1, String obj2) {
 	    double a = Double.parseDouble(obj1);
@@ -69,15 +69,15 @@ public class DataDescriptor {
 	    inputs.add(elements[col]);
 	}
 	reader.close();
-	
-	if (col == 8) {
-	    for(int i = 0; i < inputs.size(); i++) {
-		inputs.set(i, String.valueOf(Double.parseDouble(inputs.get(i)) * (9.0/5.0) + 32.0));
+
+	if (col == 8) { // Fixing crappy temp values
+	    for (int i = 0; i < inputs.size(); i++) {
+		inputs.set(i, String.valueOf(Double.parseDouble(inputs.get(i)) * (9.0 / 5.0) + 32.0));
 	    }
 	}
 
 	double sum = 0;
-	// inputs.sort(new IntStringComparator());
+	inputs.sort(new IntStringComparator());
 
 	// Generate range
 	// Split into 10 buckets
@@ -86,44 +86,103 @@ public class DataDescriptor {
 	double minval = Double.parseDouble(inputs.get(0));
 	double maxval = Double.parseDouble(inputs.get(inputs.size() - 1));
 	double bucketsize = (maxval - minval) / 10.0;
-	
-	
+
 	for (String s : inputs) {
 	    double value = Double.parseDouble(s);
-	    
-	    buckets[(int)((Math.abs((value - minval)/maxval) * 10) % 10)]++;
+
+	    buckets[(int) ((Math.abs((value - minval) / maxval) * 10) % 10)]++;
 
 	    sum += value;
 	}
 
 	for (int i = 0; i < 10; i++) {
-	    System.out.println("(" + i + ") " + String.format("%.2f", minval + bucketsize * i) + " - " + String.format("%.2f", minval + bucketsize * (i+1)) + ": " + buckets[i] + 
-		    " (" + String.format("%.2f", buckets[i] * 100.0 / inputs.size()) + "%)");
+	    System.out.println("(" + i + ") " + String.format("%.2f", minval + bucketsize * i) + " - " + String.format("%.2f", minval + bucketsize * (i + 1)) + ": " + buckets[i] + " (" + String.format("%.2f", buckets[i] * 100.0 / inputs.size()) + "%)");
 	}
-	
+
 	double mean = sum / inputs.size();
 	double sumdifferencessquared = 0;
 	for (String s : inputs) {
 	    double value = Double.parseDouble(s);
-	    
+
 	    sumdifferencessquared += (mean - value) * (mean - value);
-	    
+
 	}
-	
+
 	double variance = sumdifferencessquared / inputs.size();
-	
+
 	System.out.println("Variance: " + variance);
 	System.out.println("Standard Deviation: " + Math.sqrt(variance));
 	System.out.println("Mean: " + mean);
 	System.out.println("Median: " + inputs.get(inputs.size() / 2));
     }
 
+    public void describeTmeColumn(int col) throws IOException {
+   	BufferedReader reader = new BufferedReader(new FileReader(_fileName));
+
+   	ArrayList<String> inputs = new ArrayList<>();
+
+   	String line;
+   	reader.readLine(); // remove headers.
+
+   	while ((line = reader.readLine()) != null) {
+   	    String[] elements = line.split(",");
+   	    inputs.add(elements[col]);
+   	}
+   	reader.close();
+
+   	if (col == 8) { // Fixing crappy temp values
+   	    for (int i = 0; i < inputs.size(); i++) {
+   		inputs.set(i, String.valueOf(Double.parseDouble(inputs.get(i)) * (9.0 / 5.0) + 32.0));
+   	    }
+   	}
+
+   	double sum = 0;
+   	inputs.sort(new IntStringComparator());
+
+   	// Generate range
+   	// Split into 10 buckets
+   	// Fill
+   	int[] buckets = new int[10];
+   	double minval = Double.parseDouble(inputs.get(0));
+   	double maxval = Double.parseDouble(inputs.get(inputs.size() - 1));
+   	double bucketsize = (maxval - minval) / 10.0;
+
+   	for (String s : inputs) {
+   	    double value = Double.parseDouble(s);
+
+   	    buckets[(int) ((Math.abs((value - minval) / maxval) * 10) % 10)]++;
+
+   	    sum += value;
+   	}
+
+   	for (int i = 0; i < 10; i++) {
+   	    System.out.println("(" + i + ") " + String.format("%.2f", minval + bucketsize * i) + " - " + String.format("%.2f", minval + bucketsize * (i + 1)) + ": " + buckets[i] + " (" + String.format("%.2f", buckets[i] * 100.0 / inputs.size()) + "%)");
+   	}
+
+   	double mean = sum / inputs.size();
+   	double sumdifferencessquared = 0;
+   	for (String s : inputs) {
+   	    double value = Double.parseDouble(s);
+
+   	    sumdifferencessquared += (mean - value) * (mean - value);
+
+   	}
+
+   	double variance = sumdifferencessquared / inputs.size();
+
+   	System.out.println("Variance: " + variance);
+   	System.out.println("Standard Deviation: " + Math.sqrt(variance));
+   	System.out.println("Mean: " + mean);
+   	System.out.println("Median: " + inputs.get(inputs.size() / 2));
+       }
+
+    
     public DataDescriptor(String fileName) throws Exception {
 	_fileName = fileName;
     }
 
     public static void main(String[] args) throws Exception {
-	DataDescriptor dr = new DataDescriptor("data/data.csv");
+	DataDescriptor dr = new DataDescriptor("/home/romek/projects/timelapseweather/data/data.csv"); // "D:\\Dropbox\\Dropbox\\Part II\\project\\data\\data.csv");
 	System.out.println("-- ICON --");
 	dr.describeDescreteColumn(3);
 	System.out.println();
@@ -132,21 +191,14 @@ public class DataDescriptor {
 	dr.describeContinuousColumn(6);
 	System.out.println();
 
-	
 	System.out.println("-- PRECIPITATION --");
 	dr.describeContinuousColumn(7);
 	System.out.println();
 
-	
 	System.out.println("-- TEMPERATURE--");
 	dr.describeContinuousColumn(8);
 	System.out.println();
 
-	dr = new DataDescriptor("data/datamerged.csv");
-	System.out.println("-- CLOUD COVER --");
-	dr.describeContinuousColumn(14 );
-	System.out.println();
-	
     }
 
 }
